b494702f651e3e63ce445306e5745e4bb7313cf8
tayshop-jona <tayshopof@gmail.com>
2026-05-22 22:19:50 -0600

    Update Groq model to llama-3.1-70b-versatile

--- api/ai.js
+++ api/ai.js
@@ -46,7 +46,7 @@ module.exports = async (req, res) => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        model: 'mixtral-8x7b-32768',
+        model: 'llama-3.1-70b-versatile',
         messages: [{ role: 'user', content: userPrompt }],
         temperature: 0.7,
         max_tokens: 1024
